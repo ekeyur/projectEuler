a431bbe57c0b2acc23d1c3f611ffe1db9a74f46a
Keyur Patel <keyur@Keyurs-MacBook-Pro.local>
2016-12-19 21:07:35 -0500

    problem 4

---
+++ problem4.py
@@ -0,0 +1,16 @@
+n=999
+palin = []
+
+for i in range(100,1000):
+    for j in range(i,1000):
+        num = i * j
+        n = num
+        rev = 0
+        while(num > 0):
+            dig = num % 10
+            rev = rev * 10 + dig
+            num = num / 10
+        if rev == n:
+            palin.append(n)
+
+print max(palin)            
