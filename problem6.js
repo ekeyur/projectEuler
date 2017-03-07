eec924cbe3df9a4ee3a8d160b887775685b6d1fc
Keyur Patel <ekeyur@gmail.com>
2017-03-07 10:30:02 -0500

    completed problem 6

--- problem6.js
+++ problem6.js
@@ -1,27 +1,39 @@
-var n = 10;
+// The sum of the squares of the first ten natural numbers is,
+//
+// 12 + 22 + ... + 102 = 385
+// The square of the sum of the first ten natural numbers is,
+//
+// (1 + 2 + ... + 10)2 = 552 = 3025
+// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+//
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
-function isPrime(x){
-  for(var i=2;i<Math.sqrt(x);i++){
-    if (x % i === 0){
-      return false;
-    }
-  }
-  return true;
-}
-var k = [];
-for(var i=2;i<=n;i++){
-  k.push(i);
-}
-var product = 1;
-var primes = k.filter(isPrime);
 
-console.log(primes);
+var arr = range(1,101);
 
-primes.forEach(function(x){
-  while(x*x < n){
-    x = x * x;
-  }
-  product = product * x;
+var square_sum = arr.map(function(n){
+  return n * n;
+}).reduce(function(a,b){
+  return a+b;
 });
 
-console.log(product);
+var sum_arr = arr.reduce(function(a,b){
+  return a+b;
+});
+
+console.log((sum_arr * sum_arr)-square_sum);
+
+
+
+
+
+
+
+
+function range(min, max) {
+  var arr = [];
+  for (var i = min; i < max; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
